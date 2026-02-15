e98c66f8b051ff04b77fff3b9247be11dfd58a75
snoofee <snoofee@cafe.com>
2026-02-15 09:26:01 +0900

    **

--- worker.js
+++ worker.js
@@ -1,5 +1,28 @@
 export default {
-  async fetch(req) {
-    return new Response("Worker running!");
+  async fetch(request) {
+    const url = new URL(request.url);
+
+    // LOGIN API
+    if (url.pathname === "/api/login" && request.method === "POST") {
+      const body = await request.json();
+
+      const { username, password } = body;
+
+      if (username === "admin" && password === "1234") {
+        return new Response(JSON.stringify({
+          success: true,
+          message: "Login OK"
+        }), {
+          headers: { "Content-Type": "application/json" }
+        });
+      }
+
+      return new Response(JSON.stringify({
+        success: false,
+        message: "Invalid"
+      }), { status: 401 });
+    }
+
+    return new Response("Not Found", { status: 404 });
   }
 };
